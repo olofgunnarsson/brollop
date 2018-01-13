7333bc02bcec2e85ee2383cb7ed063b0a613d7f0
Olof <olof.jp.gunnarsson@gmail.com>
2018-01-13 16:45:18 +0100

    Use "Natural gallery"

--- index.js
+++ index.js
@@ -1,89 +1,28 @@
 void function (window, document) {
     var nextPage = 1;
-    var thumbnails = $('.fk-thumbnails');
-    var pager = $('.pg-pager');
-    var lightbox = $('.lb-container');
-    var lightboxTitle = $('.lb-title', lightbox);
-    var lightboxImage = $('.lb-image', lightbox);
-    var lightboxPrev = $('.lb-prev', lightbox);
-    var lightboxNext = $('.lb-next', lightbox);
-    var selection;
+
+
+    var galleryData = {
+        options: {
+            showCount: false,
+            searchFilter: true,
+            categoriesFilter: false,
+            showNone: true,
+            showOthers: true
+        }
+    };
+
+    var galleryEl = document.getElementById('gallery');
+    var photoswipeEl = document.getElementsByClassName('pswp')[0];
+    var gallery = new NaturalGallery.Gallery(galleryEl, photoswipeEl, galleryData);
+
+    gallery.images = [];
 
     more();
-    thumbnails.addEventListener('click', clickedThumbnail);
-    pager.addEventListener('click', clickedPager);
-    lightbox.addEventListener('click', hide);
-    lightboxPrev.addEventListener('click', stop(prev));
-    lightboxNext.addEventListener('click', stop(next));
-    document.addEventListener('keydown', shortcuts);
+
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('serviceworker.js');
-    }
-
-    function clickedThumbnail(e) {
-        var el = e.target;
-        if (el.tagName === 'IMG') {
-            highlight(el);
-        }
-    }
-
-    function clickedPager() {
-        setClass(pager, 'pg-hidden', true);
-        more();
-    }
-
-    function shortcuts(e) {
-        if (!selection) {
-            return;
-        }
-        var esc = 27;
-        var left = 37;
-        var right = 39;
-        if (left === e.which) {
-            prev();
-        } else if (right === e.which) {
-            next();
-        } else if (esc === e.which) {
-            hide();
-        }
-    }
-
-    function stop(fn) {
-        return function (e) {
-            e.stopPropagation();
-            return fn.apply(this, arguments);
-        };
-    }
-
-    function prev() {
-        if (selection.previousSibling) {
-            highlight(selection.previousSibling);
-        }
-    }
-
-    function next() {
-        if (selection.nextSibling) {
-            highlight(selection.nextSibling);
-        }
-    }
-
-    function invalidate() {
-        setClass(lightboxPrev, 'lb-disabled', !selection.previousSibling);
-        setClass(lightboxNext, 'lb-disabled', !selection.nextSibling);
-    }
-
-    function highlight(thumb) {
-        text(lightboxTitle, thumb.getAttribute('data-title'));
-        lightboxImage.src = thumb.getAttribute('data-large');
-        selection = thumb;
-        setClass(lightbox, 'lb-show', true);
-        invalidate();
-    }
-
-    function hide() {
-        selection = null;
-        setClass(lightbox, 'lb-show', false);
     }
 
     function more() {
@@ -98,23 +37,33 @@ void function (window, document) {
 //        user_id: '154685165@N08',
             user_id: '78621811@N06',
             page: page,
-            per_page: 50
+            per_page: 30
         };
         var url = 'https://api.flickr.com/services/rest';
         jsonp(url, query, renderPhotos);
     }
 
     function renderPhotos(data) {
-        data.photos.photo.forEach(renderPhoto);
-        setClass(pager, 'pg-hidden', false);
+        var map = data.photos.photo.map(renderPhoto);
+        gallery.appendItems(map);
+
     }
 
     function renderPhoto(photo) {
-        var thumb = tag('img', 'fk-photo');
-        thumb.src = buildFlickrPhotoUrl(photo, '_q');
-        thumb.setAttribute('data-large', buildFlickrPhotoUrl(photo));
-        thumb.setAttribute('data-title', photo.title);
-        thumbnails.appendChild(thumb);
+        var thumbSrc = buildFlickrPhotoUrl(photo, '_n');
+        var orgSrc = buildFlickrPhotoUrl(photo, '_b');
+        var image = {
+            "thumbnail": thumbSrc,
+            "enlarged": orgSrc,
+            "title": photo.title,
+            "categories": [],
+            "tWidth": 75,
+            "tHeight": 75,
+            "eWidth": 0,
+            "eHeight": 0
+        };
+        console.info("Added image");
+        return image;
     }
 
     function buildFlickrPhotoUrl(photo, postfix) {
@@ -123,7 +72,7 @@ void function (window, document) {
             '.staticflickr.com/', photo.server,
             '/', photo.id,
             '_', photo.secret,
-            postfix || '_z',
+            postfix || '',
             '.jpg'].join('');
     }
 
@@ -133,10 +82,6 @@ void function (window, document) {
             el.className = className;
         }
         return el;
-    }
-
-    function text(el, value) {
-        el.textContent = el.innerText = value;
     }
 
     function jsonp(url, query, done) {
@@ -155,11 +100,4 @@ void function (window, document) {
         }
     }
 
-    function setClass(el, className, enabled) {
-        el.classList[enabled ? 'add' : 'remove'](className);
-    }
-
-    function $(selector, context) {
-        return (context || document).querySelector(selector);
-    }
 }(window, document);
